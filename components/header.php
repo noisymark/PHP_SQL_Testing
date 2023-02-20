37dbe8c3ae09188870abe8eb22904cc73ccfffed
Marko Pavlović <markopavlovic316@gmail.com>
2023-02-20 14:47:05 +0100

    Adding favicon

--- components/header.php
+++ components/header.php
@@ -2,7 +2,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LoveCalculator</title>
+    <title>BlueFreedom</title>
     <link rel="stylesheet" href="components/css/bootstrap.css">
     <link rel="stylesheet" href="components/css/app.css">
+    <link rel="shortcut icon" href="components/img/favicon.ico" type="image/x-icon">
 </head>
